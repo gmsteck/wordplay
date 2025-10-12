6489649cf96b235138c9f8dbf3341e8eda622551
Gavin Steck <gavinmsteck@gmail.com>
2025-10-11 21:36:40 -0600

    Update user page logic to show the correct username

--- lib/provider/game_list_provider.dart
+++ lib/provider/game_list_provider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:sample/controller/game-List_controller.dart';
+import 'package:sample/controller/game_list_controller.dart';
 import 'package:sample/model/wordle_game_model.dart';
 import 'package:sample/provider/user_provider.dart';
 import 'package:sample/service/game_service.dart';
--- lib/view/user.dart
+++ lib/view/user.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sample/provider/auth_provider.dart';
-import 'base_page.dart';
+import 'package:sample/provider/game_list_provider.dart';
+import 'package:sample/view/base_page.dart';
 
 class UserPage extends ConsumerWidget {
   const UserPage({super.key});
@@ -10,7 +12,6 @@ class UserPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(authControllerProvider).user;
 
-    // Trigger navigation after the build is done if user is null
     if (user == null) {
       return const Scaffold(
         backgroundColor: Colors.white,
@@ -18,55 +19,68 @@ class UserPage extends ConsumerWidget {
       );
     }
 
-    return BasePage(
-      title: 'User Info',
-      child: SingleChildScrollView(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.only(bottom: 16),
-              child: CircleAvatar(
-                radius: 56,
-                backgroundImage: NetworkImage(user.pictureUrl.toString()),
-              ),
-            ),
-            Card(
-              color: Colors.white,
-              elevation: 4,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Column(
-                children: [
-                  _userEntry('ID', user.id),
-                  _userEntry('Name', user.name),
-                  _userEntry('Email', user.email),
-                  _userEntry('Updated At', user.lastUpdated.toIso8601String()),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+    // async fetch inside FutureBuilder
+    final futureUsername = ref
+        .read(gameListControllerProvider(user.id).notifier)
+        .getUsername(user.id);
 
-  Widget _userEntry(String label, String? value) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-      margin: const EdgeInsets.symmetric(vertical: 2),
-      color: Colors.white,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(label, style: const TextStyle(color: Colors.black)),
-          Flexible(
-            child:
-                Text(value ?? '', style: const TextStyle(color: Colors.black)),
+    return FutureBuilder<String>(
+      future: futureUsername,
+      builder: (context, snapshot) {
+        final username = snapshot.connectionState == ConnectionState.done
+            ? snapshot.data ?? 'Unknown'
+            : 'Loading...';
+
+        return BasePage(
+          title: 'User Info',
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 56,
+                  backgroundImage: NetworkImage(user.pictureUrl.toString()),
+                ),
+                Card(
+                  color: Colors.white,
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Column(
+                    children: [
+                      _userEntry('ID', user.id),
+                      _userEntry('Name', username),
+                      _userEntry('Email', user.email),
+                      _userEntry(
+                        'Updated At',
+                        user.lastUpdated.toIso8601String(),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-        ],
-      ),
+        );
+      },
     );
   }
+}
+
+Widget _userEntry(String label, String? value) {
+  return Container(
+    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+    margin: const EdgeInsets.symmetric(vertical: 2),
+    color: Colors.white,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(label, style: const TextStyle(color: Colors.black)),
+        Flexible(
+          child: Text(value ?? '', style: const TextStyle(color: Colors.black)),
+        ),
+      ],
+    ),
+  );
 }
